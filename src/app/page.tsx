90f37bc71a5786af2afcb459cfcc310e9858fa37
Andriikryl <andreyk557@gmail.com>
2023-11-15 16:42:07 +0200

    add bg styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>asd</h1>
+      <h1>hell ye</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci
+        dolore impedit ut veritatis perferendis aspernatur error, beatae
+        asperiores. Dolore, perspiciatis nobis provident consectetur fuga
+        debitis laboriosam deleniti corrupti hic!
+      </p>
     </div>
   );
 }
